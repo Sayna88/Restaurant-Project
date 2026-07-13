3a6f40c55e8def3a8b444c01dd033af9a9f3a811
Sayna <saynashakoury79@gmail.com>
2026-07-13 23:50:17 +0330

    finish menus

--- src/components/data/CafeData.ts
+++ src/components/data/CafeData.ts
@@ -132,54 +132,508 @@ export const cafeMenu = [
   },
 
   // --- Teas and Herbal ---
-
+  
   {
     id: 17,
     categoryId: "Teas",
-    name: "Baharnarenj Tea",
-    ingredients: ["Baharnarenj", "Cinnamon stick", "Saffron"],
-    price: 220000,
+    name: "Orange Blossom Herbal Tea",
+    ingredients: ["Orange blossom", "Cinnamon stick", "Saffron"],
+    price: 260000,
     image: "/t1.webp"
   },
   {
     id: 18,
     categoryId: "Teas",
-    name: "Tea and Saffron",
+    name: "Saffron Tea",
     ingredients: [],
-    price: 220000,
+    price: 280000,
     image: "/t2.webp"
   },
   {
     id: 19,
     categoryId: "Teas",
-    name: "Hibiscus tea",
-    ingredients: ["Hibiscus tea", "Coriander seeds", "Valerian"],
-    price: 220000,
+    name: "Hibiscus Tea",
+    ingredients: ["Hibiscus", "Coriander seeds", "Valerian root"],
+    price: 270000,
     image: "/t3.webp"
   },
   {
     id: 20,
     categoryId: "Teas",
-    name: "Sun-Infused Brew",
-    ingredients: ["Chamomile", "French Lavender", "Mango", "Yellow rose"],
-    price: 220000,
+    name: "Sun Brew Herbal Tea",
+    ingredients: ["Chamomile", "French lavender", "Mango", "Yellow rose"],
+    price: 270000,
     image: "/t4.webp"
   },
   {
     id: 21,
     categoryId: "Teas",
-    name: "Tea and Cinnamon",
+    name: "Cinnamon Tea",
     ingredients: [],
-    price: 180000,
+    price: 250000,
     image: "/t5.webp"
   },
   {
     id: 22,
     categoryId: "Teas",
-    name: "Blue Butterfly Infused",
-    ingredients: ["French lavender", "butterfly pea", "lemongrass", "blueberry"],
-    price: 180000,
+    name: "Blue Butterfly Pea Brew",
+    ingredients: ["French lavender", "Butterfly pea", "Lemongrass", "Blueberry"],
+    price: 270000,
     image: "/t6.webp"
   },
+  {
+    id: 23,
+    categoryId: "Teas",
+    name: "Plain Tea",
+    ingredients: [],
+    price: 185000,
+    image: "/t7.webp"
+  },
+  {
+    id: 24,
+    categoryId: "Teas",
+    name: "Lemongrass Tea",
+    ingredients: ["Lemon", "Ginger", "Mint"],
+    price: 270000,
+    image: "/t8.webp"
+  },
+  {
+    id: 25,
+    categoryId: "Teas",
+    name: "Qaleh Rudkhan Handcrafted Tea",
+    ingredients: [],
+    price: 300000,
+    image: "/t9.webp"
+  },
+  {
+    id: 26,
+    categoryId: "Teas",
+    name: "Mixed Berry Tea",
+    ingredients: ["Strawberry", "Barberry", "Cranberry"],
+    price: 270000,
+    image: "/t9.webp"
+  },
+  {
+    id: 27,
+    categoryId: "Teas",
+    name: "Amlash Smoked Tea",
+    ingredients: [],
+    price: 300000,
+    image: "/t9.webp"
+  },
+  {
+    id: 28,
+    categoryId: "Teas",
+    name: "Fuman Handcrafted Tea",
+    ingredients: [],
+    price: 300000,
+    image: "/t10.webp"
+  },
+  {
+    id: 29,
+    categoryId: "Teas",
+    name: "Green Tea",
+    ingredients: [],
+    price: 280000,
+    image: "/t11.webp"
+  },
+  {
+    id: 30,
+    categoryId: "Teas",
+    name: "Jasmine Green Tea",
+    ingredients: [],
+    price: 280000,
+    image: "/t11.webp"
+  },
+  {
+    id: 31,
+    categoryId: "Teas",
+    name: "Peppermint Tea",
+    ingredients: [],
+    price: 280000,
+    image: "/t11.webp"
+  },
+  // --- Milkshakes ---
+  {
+    id: 32,
+    categoryId: "Milkshakes",
+    name: "Nutella Strawberry",
+    ingredients: ["Vanilla ice cream", "Strawberry", "Nutella", "Milk", "Cream"],
+    price: 570000,
+    image: "/m1.webp"
+  },
+  {
+    id: 33,
+    categoryId: "Milkshakes",
+    name: "Nescafe",
+    ingredients: ["Nescafe ice cream", "Milk", "Cream"],
+    price: 490000,
+    image: "/m2.webp"
+  },
+  {
+    id: 34,
+    categoryId: "Milkshakes",
+    name: "Strawberry",
+    ingredients: ["Strawberry ice cream", "Milk", "Cream"],
+    price: 450000,
+    image: "/m3.webp"
+  },
+  {
+    id: 35,
+    categoryId: "Milkshakes",
+    name: "Oreo Shake",
+    ingredients: ["Vanilla ice cream", "Oreo biscuit", "Milk", "Cream"],
+    price: 550000,
+    image: "/m4.webp"
+  },
+  {
+    id: 36,
+    categoryId: "Milkshakes",
+    name: "Nutella Banana",
+    ingredients: ["Vanilla ice cream", "Banana", "Nutella", "Milk", "Cream"],
+    price: 585000,
+    image: "/m5.jpg"
+  },
+  {
+    id: 37,
+    categoryId: "Milkshakes",
+    name: "Banana Chocolate",
+    ingredients: ["Chocolate ice cream", "Banana", "Milk", "Cream"],
+    price: 525000,
+    image: "/m6.webp"
+  },
+  {
+    id: 38,
+    categoryId: "Milkshakes",
+    name: "Nutella",
+    ingredients: ["Vanilla ice cream", "Nutella", "Milk", "Cream"],
+    price: 520000,
+    image: "/m7.webp"
+  },
+  {
+    id: 39,
+    categoryId: "Milkshakes",
+    name: "KitKat",
+    ingredients: ["Chocolate ice cream", "KitKat chocolate", "Milk", "Cream"],
+    price: 555000,
+    image: "/m8.webp"
+  },
+  {
+    id: 40,
+    categoryId: "Milkshakes",
+    name: "Nutella Sorbon",
+    ingredients: ["Vanilla ice cream", "Nutella", "Sorbon", "Milk", "Cream"],
+    price: 480000,
+    image: "/m9.webp"
+  },
+  {
+    id: 41,
+    categoryId: "Milkshakes",
+    name: "Snickers",
+    ingredients: ["Chocolate ice cream", "Snickers", "Milk", "Cream"],
+    price: 555000,
+    image: "/m10.jpg"
+  },
 
+  // --- Smoothies ---
+
+  {
+    id: 42,
+    categoryId: "Smoothies",
+    name: "Seasonal Juice",
+    ingredients: [],
+    price: 520000,
+    image: "/sm1.webp"
+  },
+  {
+    id: 43,
+    categoryId: "Smoothies",
+    name: "Cornelian Cherry & Raspberry Smoothie",
+    ingredients: ["Cornelian cherry", "Raspberry", "Lemon", "Mixed red fruits"],
+    price: 440000,
+    image: "/sm2.webp"
+  },
+  {
+    id: 44,
+    categoryId: "Smoothies",
+    name: "Mango Passion Fruit Smoothie",
+    ingredients: ["Mango", "Passion fruit", "Orange", "Pineapple"],
+    price: 410000,
+    image: "/sm3.webp"
+  },
+  {
+    id: 45,
+    categoryId: "Smoothies",
+    name: "Apple Banana Smoothie",
+    ingredients: ["Green apple", "Banana", "Apple juice"],
+    price: 440000,
+    image: "/sm4.webp"
+  },
+  {
+    id: 46,
+    categoryId: "Smoothies",
+    name: "Tropical Smoothie",
+    ingredients: ["Mango", "Banana", "Strawberry", "Peach", "Pineapple"],
+    price: 555000,
+    image: "/sm5.webp"
+  },
+  {
+    id: 47,
+    categoryId: "Smoothies",
+    name: "Lavashak Smoothie",
+    ingredients: ["Lavashak", "Barberry juice", "Ice"],
+    price: 440000,
+    image: "/sm6.webp"
+  },
+  {
+    id: 48,
+    categoryId: "Smoothies",
+    name: "Lemonade",
+    ingredients: ["Lemon", "Ice", "Soda"],
+    price: 260000,
+    image: "/sm7.webp"
+  },
+  {
+    id: 49,
+    categoryId: "Smoothies",
+    name: "Mojito Smoothie",
+    ingredients: ["Lemon", "Mint", "Ice"],
+    price: 295000,
+    image: "/sm8.webp"
+  },
+  {
+    id: 50,
+    categoryId: "Smoothies",
+    name: "Seasonal Mojito",
+    ingredients: [],
+    price: 480000,
+    image: "/sm9.webp"
+  },
+
+  // --- Ice Coffee ---
+  
+  {
+    id: 51,
+    categoryId: "IceCoffee",
+    name: "Iced Americano",
+    ingredients: ["Espresso", "Water", "Ice"],
+    price: 200000,
+    image: "/i1.jpg"
+  },
+  {
+    id: 52,
+    categoryId: "IceCoffee",
+    name: "Frappuccino",
+    ingredients: ["Espresso", "Vanilla ice cream", "Cream", "Ice"],
+    price: 500000,
+    image: "/i2.webp"
+  },
+  {
+    id: 53,
+    categoryId: "IceCoffee",
+    name: "Iced Latte",
+    ingredients: ["Espresso", "Milk", "Ice"],
+    price: 300000,
+    image: "/i3.jpg"
+  },
+  {
+    id: 54,
+    categoryId: "IceCoffee",
+    name: "Iced Cocolatte",
+    ingredients: ["Coconut milk", "Coffee", "Ice"],
+    price: 350000,
+    image: "/i4.jpg"
+  },
+  {
+    id: 55,
+    categoryId: "IceCoffee",
+    name: "Iced Mocha",
+    ingredients: ["Espresso", "Milk", "Chocolate", "Ice"],
+    price: 350000,
+    image: "/i5.webp"
+  },
+  {
+    id: 56,
+    categoryId: "IceCoffee",
+    name: "Iced Coconut Mocha",
+    ingredients: ["Coconut milk", "Coffee", "Chocolate sauce"],
+    price: 395000,
+    image: "/i6.webp"
+  },
+  {
+    id: 57,
+    categoryId: "IceCoffee",
+    name: "Iced Snickers",
+    ingredients: ["Milk", "Coffee", "Ice", "Caramel syrup", "Peanut butter"],
+    price: 400000,
+    image: "/i7.jpg"
+  },
+  {
+    id: 58,
+    categoryId: "IceCoffee",
+    name: "Iced Hazelnut Mocha",
+    ingredients: ["Milk", "Coffee", "Chocolate sauce", "Hazelnut syrup"],
+    price: 390000,
+    image: "/i8.webp"
+  },
+  {
+    id: 59,
+    categoryId: "IceCoffee",
+    name: "Iced Caramel Macchiato",
+    ingredients: ["Small amount of milk", "Coffee", "Caramel syrup"],
+    price: 350000,
+    image: "/i7.jpg"
+  },
+
+  // --- Desserts ---
+  
+  {
+    id: 60,
+    categoryId: "Desserts",
+    name: "Cake of the Day",
+    ingredients: [],
+    price: 320000,
+    image: "/de1.jpg"
+  },
+  {
+    id: 61,
+    categoryId: "Desserts",
+    name: "Ice Cream Cake",
+    ingredients: [],
+    price: 685000,
+    image: "/de2.webp"
+  },
+  {
+    id: 62,
+    categoryId: "Desserts",
+    name: "Cheesecake",
+    ingredients: [],
+    price: 350000,
+    image: "/de3.jpg"
+  },
+  {
+    id: 63,
+    categoryId: "Desserts",
+    name: "Affogato",
+    ingredients: [],
+    price: 360000,
+    image: "/de4.webp"
+  },
+  {
+    id: 64,
+    categoryId: "Desserts",
+    name: "Special Ice Cream",
+    ingredients: [],
+    price: 550000,
+    image: "/de5.webp"
+  },
+
+  // --- Waffles ---
+  {
+    id: 65,
+    categoryId: "Waffles",
+    name: "Nutella, Banana & Ice Cream Waffle",
+    ingredients: [],
+    price: 680000,
+    image: "/w1.webp"
+  },
+  {
+    id: 66,
+    categoryId: "Waffles",
+    name: "Strawberry Waffle",
+    ingredients: [],
+    price: 630000,
+    image: "/w2.webp"
+  },
+  {
+    id: 67,
+    categoryId: "Waffles",
+    name: "Peanut Butter Waffle",
+    ingredients: [],
+    price: 630000,
+    image: "/w3.webp"
+  },
+
+  // --- Mocktails ---
+
+  {
+    id: 68,
+    categoryId: "Mocktails",
+    name: "Bartender Mocktail",
+    ingredients: [],
+    price: 555000,
+    image: "/ma1.webp"
+  },
+  {
+    id: 69,
+    categoryId: "Mocktails",
+    name: "Cuba Libre",
+    ingredients: ["Lime", "Rum syrup", "Cola"],
+    price: 185000,
+    image: "/ma2.webp"
+  },
+  {
+    id: 70,
+    categoryId: "Mocktails",
+    name: "Black Mojito",
+    ingredients: ["Raspberry", "Lime", "Rum syrup", "Soda"],
+    price: 440000,
+    image: "/ma3.webp"
+  },
+  {
+    id: 71,
+    categoryId: "Mocktails",
+    name: "Batanga",
+    ingredients: ["Lime", "Agave syrup", "Cola"],
+    price: 200000,
+    image: "/ma4.webp"
+  },
+  {
+    id: 72,
+    categoryId: "Mocktails",
+    name: "Red Mojito",
+    ingredients: ["Strawberry", "Lime", "Ice mint", "Soda"],
+    price: 400000,
+    image: "/ma5.webp"
+  },
+  {
+    id: 73,
+    categoryId: "Mocktails",
+    name: "Cuban Mojito",
+    ingredients: ["Ice mint", "Lime", "Rum syrup", "Soda"],
+    price: 385000,
+    image: "/ma6.webp"
+  },
+  {
+    id: 74,
+    categoryId: "Mocktails",
+    name: "Seasonal Mojito",
+    ingredients: ["Choice of fruit", "Ice mint", "Lime", "Soda"],
+    price: 385000,
+    image: "/ma7.webp"
+  },
+  {
+    id: 75,
+    categoryId: "Mocktails",
+    name: "Piña Colada",
+    ingredients: ["Pineapple", "Coconut", "Rum syrup", "Ice"],
+    price: 435000,
+    image: "/ma8.webp"
+  },
+  {
+    id: 76,
+    categoryId: "Mocktails",
+    name: "Red Berry",
+    ingredients: ["Raspberry", "Cornelian cherry", "Plum", "Barberry", "Lime", "Mixed red syrup"],
+    price: 440000,
+    image: "/ma9.webp"
+  },
+  {
+    id: 77,
+    categoryId: "Mocktails",
+    name: "Piña Berry",
+    ingredients: ["Strawberry", "Coconut", "Pineapple"],
+    price: 500000,
+    image: "/ma10.webp"
+  }
 ];
